567a0e1310a2ff09b00ce97ba2f4f4a45a73b0d7
dariathompson <daria.kambalina@gmail.com>
2020-07-03 14:13:15 +0100

    finished printing method

--- main.py
+++ main.py
@@ -7,5 +7,16 @@ def spaceIsFree(pos):
     return board[pos] == ' '
 
 def printBoard(board):
-    print('  |  |')
+    print('   |   |')
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    
+printBoard(board)
